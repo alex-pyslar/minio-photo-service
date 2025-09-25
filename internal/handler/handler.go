93c849c29d4a33b55bc3aed28eec030734073536
alex_pyslar <alex_pyslar@DESKTOP-PCM1GDQ>
2025-09-25 18:05:34 +0300

    Починка cors для метода upload

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -77,6 +77,12 @@ func CorsMiddleware(next http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
 		next.ServeHTTP(w, r)
 	})
 }
